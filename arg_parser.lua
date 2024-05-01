83ee7a357799952266b0a3cbf7c8f439bb42ed2e
NathanSnail <47441022+NathanSnail@users.noreply.github.com>
2024-05-01 21:56:11 +1200

    options are cool\!

--- arg_parser.lua
+++ arg_parser.lua
@@ -14,6 +14,7 @@ local arg_list = {
 	nc = "number_of_casts",
 	ac = "always_casts",
 	ml = "mod_list",
+	sp = "spells",
 }
 
 local defaults = {
@@ -32,6 +33,7 @@ local defaults = {
 	number_of_casts = 1,
 	always_casts = {},
 	mod_list = {},
+	spells = {},
 }
 
 ---@param val string
@@ -48,6 +50,9 @@ end
 
 local function numeric(name)
 	return function(val)
+		if val:sub(1, 2) == ".-" then
+			val = val:sub(2)
+		end
 		local value = tonumber(val)
 		if value then
 			return math.floor(value)
@@ -56,12 +61,28 @@ local function numeric(name)
 	end
 end
 
-local function identity(...)
-	return table.unpack({ ... })
-end
-
 local function single_identity(x)
 	return x
+end
+
+local function spell_proccess(x)
+	local ptr = 1
+	local spells = {}
+	while ptr <= #x do
+		local spell = x[ptr]
+		if tonumber(spell) then
+			error("invalid spell sequence with number " .. tonumber(spell))
+		end
+		local next = x[ptr + 1]
+		local number = tonumber(next)
+		if number then
+			spell = { spell, number }
+			ptr = ptr + 1
+		end
+		table.insert(spells, spell)
+		ptr = ptr + 1
+	end
+	return spells
 end
 
 ---@type table<string, fun(...: any): any>
@@ -72,64 +93,127 @@ local arg_fns = {
 	reload_time = numeric("reload_time"),
 	cast_delay = numeric("cast_delay"),
 	number_of_casts = numeric("number_of_casts"),
-	always_casts = identity,
-	mod_list = identity,
+	always_casts = spell_proccess,
+	mod_list = single_identity,
+	spells = spell_proccess,
 }
 
-local cur_options = {}
-for k, v in pairs(defaults) do
-	cur_options[k] = v
-end
-
 --[[
+options are -... or --...
+values that are negative use .- instead of -
+arg is an option or a value
 single character options are flags, specifying them toggles from the default.
 multiple flags can be specified like -abc, it must not be followed by a value.
 you can also specify flags fully with -a true
 other values can be specified like -ma 1000, or --mods grahams_perks boss_reworks
-just use - with nothing else to specify the spells argument
-if the first arg is a value - is inserted automatically
+if no options are specified the -sp option is automatically added to the start
 ]]
 
----@param flag string
----@param value boolean?
-local function apply_flag(flag, value)
-	local short_flag = flag:sub(2, 2) ~= "-"
+---@nodiscard
+---@param option string
+---@param value string[]
+---@return any, string
+local function apply_option(option, value)
+	local short_flag = option:sub(2, 2) ~= "-"
 	if short_flag then
-		local longer = arg_list[flag:sub(2)]
+		local longer = arg_list[option:sub(2)]
 		if not longer then
-			error("unknown short flag " .. flag)
+			error("unknown short flag " .. option)
 		end
-		flag = longer
+		option = longer
 	else
-		flag = flag:sub(3)
+		option = option:sub(3)
 	end
-	if value == nil then
-		local default = defaults[flag]
-		if type(default) == "boolean" then
-			value = not default
-		else
-			error("flag " .. flag .. " requires a value")
+	if not arg_fns[option] then
+		if defaults[option] ~= nil then
+			return boolify(value[1]), option
 		end
+		error("unknown option " .. option)
 	end
-	cur_options[flag] = arg_fns[flag](value)
+	return arg_fns[option](value), option
 end
 
-if #arg > 0 then
+---@class arg_parser
+local M = {}
+
+---@class formatter_options
+---@field ansi boolean
+---@field drained boolean
+---@field every_other boolean
+---@field unlimited_spells boolean
+---@field tree boolean
+---@field counts boolean
+---@field states boolean
+---@field spells_per_cast integer
+---@field wand_file string?
+---@field mana integer
+---@field reload_time integer
+---@field cast_delay integer
+---@field number_of_casts integer
+---@field always_casts string[]
+---@field mod_list string[]
+---@field spells string[]
+
+---@param args string[]
+---@return formatter_options?
+function M.parse(args)
+	local cur_options = {}
+	for k, v in pairs(defaults) do
+		cur_options[k] = v
+	end
+
+	if #args == 0 then
+		return nil
+	end
 	local ptr = 1
-	while ptr <= #arg do
-		local cur_arg = arg[ptr]
+	local contains_opts = false
+	for _, v in ipairs(args) do
+		contains_opts = contains_opts or v:sub(1, 1) == "-"
+	end
+	if not contains_opts then
+		cur_options.spells = args
+		return cur_options
+	end
+	while ptr <= #args do
+		local cur_arg = args[ptr]
 		local is_opt = cur_arg:sub(1, 1) == "-"
 		local is_long_opt = cur_arg:sub(2, 2) == "-"
-		local next_arg = arg[ptr + 1]
-		local next_arg_is_opt
+		local is_short_opt = is_opt and not is_long_opt
+		local next_arg = args[ptr + 1]
+		local next_arg_is_opt = false
 		if next_arg then
 			next_arg_is_opt = (next_arg:sub(1, 1) == "-")
 		end
-		local flag_block = is_opt and not is_long_opt and next_arg_is_opt
+		local flag_block = is_short_opt and (not next_arg or next_arg_is_opt)
 		if flag_block then
 			for i = 2, #cur_arg do
 				local flag_char = cur_arg:sub(i, i)
+				local full_name = arg_list[flag_char]
+				if defaults[full_name] == nil then
+					error("unknown flag " .. flag_char)
+				end
+				cur_options[full_name] = not defaults[full_name]
 			end
+		elseif is_opt then
+			local parameter_list = {}
+			ptr = ptr + 1
+			while ptr <= #args do
+				local cur = args[ptr]
+				if cur:sub(1, 1) == "-" then
+					ptr = ptr - 1
+					break
+				end
+				ptr = ptr + 1
+				table.insert(parameter_list, cur)
+			end
+			local value, name = apply_option(cur_arg, parameter_list)
+			cur_options[name] = value
+		else
+			error("stray value " .. cur_arg)
 		end
+		ptr = ptr + 1
 	end
+	return cur_options
 end
+
+return M
--- main.lua
+++ main.lua
@@ -3,31 +3,24 @@ local fake_engine = require("fake_engine")
 local renderer = require("renderer")
 local text_formatter = require("text_formatter")
 print_table = require("print")
+---@type arg_parser
+local arg_parser = require("arg_parser")
+local options = arg_parser.parse(arg)
+--print_table(options)
 fake_engine.initialise_engine(text_formatter)
 
-if #arg > 0 then
-	local p = 1
-	local zerod = false
-	if arg[p] == "-c" or arg[p + 1] == "-c" then
-		zerod = true
-		p = p + 1
-	end
-	if arg[p] == "-a" or arg[p + 1] == "-a" then
-		p = p + 1
-		text_formatter.colours = true
-	end
-	while p <= #arg do
-		if tonumber(arg[p + 1]) then
-			fake_engine.easy_add(arg[p], tonumber(arg[p + 1]), zerod)
-			p = p + 1
+if options then
+	text_formatter.colours = options.ansi
+	for k, v in ipairs(options.spells) do
+		if type(v) == "table" then
+			fake_engine.easy_add(v[1], v[2], options.drained)
 		else
-			if zerod then
-				fake_engine.easy_add(arg[p], 0, true)
+			if options.drained then
+				fake_engine.easy_add(v, 0, true)
 			else
-				fake_engine.easy_add(arg[p])
+				fake_engine.easy_add(v)
 			end
 		end
-		p = p + 1
 	end
 else
 	text_formatter.colours = true
