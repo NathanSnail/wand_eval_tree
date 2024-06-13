a46538761f4af54afe9eefe87f09695857716251
NathanSnail <47441022+NathanSnail@users.noreply.github.com>
2024-06-13 18:34:38 +1200

    i forgor

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local arg_parser = require("arg_parser")
 local options = arg_parser.parse(arg)
 
 local fake_engine = require("fake_engine")
-fake_engine.data_path = "/home/nathan/Documents/code/noitadata/"
+fake_engine.data_path = "~/nathan/Documents/code/noitadata/"
 fake_engine.mods_path = "~/.local/share/Steam/steamapps/common/Noita/"
 fake_engine.make_fake_api(options)
 
