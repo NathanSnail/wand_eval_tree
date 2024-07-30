5986e5b6d1496fb9494834357cd0461b3efe9427
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-07-30 20:02:06 +1200

    stuffs

--- main.lua
+++ main.lua
@@ -13,12 +13,12 @@ local text_formatter = require("text_formatter")
 ---@diagnostic disable-next-line: lowercase-global
 print_table = require("print")
 local mod_interface = require("mod_interface")
----@type image
-local image = require("image")
+-- ---@type image
+--local image = require("image")
 mod_interface.load_mods(options.mods)
 fake_engine.initialise_engine(text_formatter, options)
 
 text_formatter.set_colours(options.ansi)
 fake_engine.evaluate(options)
-image.render(fake_engine.calls, options.spells)
+--image.render(fake_engine.calls, options.spells)
 print(renderer.render(fake_engine.calls, fake_engine, text_formatter, options))
