d134bd28271efeecaa61bc78f92700757db1e322
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-12-19 12:23:59 +1300

    add user config

---
+++ user_config.lua
@@ -0,0 +1,9 @@
+-- this file will override the defaults the arg parser uses, so you can specify data path easily / other common settings easily.
+
+---@type config
+local user_config = {
+	--mods_path = "/home/wand_crafter/Noita/"
+	--data_path = "/home/wand_crafter/data/"
+}
+
+return user_config
