8c7c6b5e850d8c80a191e385ecf88395df6078ea
Nathan <nathan.titirangi@gmail.com>
2025-09-20 22:02:26 +1200

    fix user config

--- user_config.lua
+++ user_config.lua
@@ -1,6 +1,7 @@
+---@diagnostic disable: missing-fields
 -- this file will override the defaults the arg parser uses, so you can specify data path easily / other common settings easily.
 
----@type config
+---@type options
 local user_config = {
 	noita_path = "/home/wand_crafter/Noita/",
 	data_path = "/home/wand_crafter/noitadata/",
